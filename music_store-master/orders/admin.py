a5e751635a5364497f539f7af4e29bdc3d555c1c
Ziyodulla-Abdukarimov <ZiyodullaDeveloper@gmail.com>
2023-05-21 17:30:29 +0500

    bug fixed

--- music_store-master/orders/admin.py
+++ music_store-master/orders/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Order, OrderItems
 
-# Register your models here.
-admin.site.register(Order)
-admin.site.register(OrderItems)
+
+class OrderInline(admin.TabularInline):
+    model = OrderItems
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderInline]
+
+
+admin.site.register(Order, OrderAdmin)
